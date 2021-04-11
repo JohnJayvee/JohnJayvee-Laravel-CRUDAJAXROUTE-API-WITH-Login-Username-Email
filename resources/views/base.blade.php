2f9b65a28da512251ee09e2b22d636457e255aa4
Jayvee Siuagan <johnjayveesiuagan@gmail.com>
2021-04-12 00:05:30 +0800

    FINAL

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -4,22 +4,30 @@
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Laravel 8 & MySQL CRUD Tutorial</title>
-    {{-- <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css" /> --}}
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
-        integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"/>
+    {{-- <link href="https://cdn.datatables.net/1.10.21/css/jquery.dataTables.min.css" rel="stylesheet"> --}}
+    <link href="https://cdn.datatables.net/1.10.21/css/dataTables.bootstrap4.min.css" rel="stylesheet">
+
 </head>
 
 <body>
     <div class="container">
         @yield('main')
     </div>
-    {{-- <script src="{{ asset('js/app.js') }}" type="text/js"></script> --}}
-    <script
-			  src="https://code.jquery.com/jquery-3.6.0.min.js"
-			  integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
-			  crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"
-        integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.0/jquery.validate.js"></script>
+<script src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+<script src="https://cdn.datatables.net/1.10.21/js/dataTables.bootstrap4.min.js"></script>
+
+<script>
+    $(function(){
+      $("#example").dataTable({
+        responsive: true
+      });
+    })
     </script>
 </body>
 
--- resources/views/contacts/index.blade.php
+++ resources/views/contacts/index.blade.php
@@ -4,7 +4,7 @@
     <div class="row">
         <div class="col-sm-12">
             <h1 class="display-3">Contacts</h1>
-            <table class="table table-striped">
+            <table id="example" class="table table-striped table-bordered" style="width:100%">
                 <thead>
                     <tr>
                         <td>ID</td>
@@ -13,7 +13,8 @@
                         <td>Job Title</td>
                         <td>City</td>
                         <td>Country</td>
-                        <td colspan=3>Actions</td>
+                        <td colspan="3" style="text-align: center;">Action</td>
+                        <td hidden></td>
                     </tr>
                 </thead>
                 <tbody>
@@ -25,19 +26,21 @@
                             <td>{{ $contact->job_title }}</td>
                             <td>{{ $contact->city }}</td>
                             <td>{{ $contact->country }}</td>
-                            <td>
+                            <td white-space: pre;>
                                 <a href="{{ route('contact.show', $contact->id) }}" class="btn btn-warning">Show</a>
                             </td>
+
                             <td>
                                 <a href="{{ route('contact.edit', $contact->id) }}" class="btn btn-primary">Edit</a>
                             </td>
                             <td>
-                                <form method="post" action="{{ route('contact.destroy', $contact->id) }}" >
+                                <form method="post" action="{{ route('contact.destroy', $contact->id) }}">
                                     @csrf
                                     @method('DELETE')
                                     <button class="btn btn-danger" type="submit">Delete</button>
                                 </form>
                             </td>
+                            <td hidden></td>
                         </tr>
                     @endforeach
                 </tbody>
